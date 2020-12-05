d2bf9894f3d23c57bfe1fa5e11c88f6d1bff527d
GustavinTM <71935384+GustavinTM@users.noreply.github.com>
2020-12-05 19:12:43 -0300

    Create Embeds.py

---
+++ Códigos/Embeds.py
@@ -0,0 +1,8 @@
+# Embed Simples
+embed = discord.Embed(
+  title="titulo aqui",
+  description="descrição aqui",
+  color=cor aqui # Utilize códigos de cor aqui, pode ser rgb, entre outros :B
+  )
+# Enviar o Embed
+await ctx.send(embed=embed)
